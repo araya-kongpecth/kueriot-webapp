c7d571772fb0e42b24d100e9d710b50ce635bd68
Uraibeef K <uraibeefk@192.168.2.39>
2022-10-10 22:58:45 +0700

    Updates

---
+++ precache-manifest.3667ec61c15276239c155dd4ef5ff778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13faad7db5e65d322990",
+    "url": "/kueriot-webapp/static/css/main.ccbfe6d4.chunk.css"
+  },
+  {
+    "revision": "13faad7db5e65d322990",
+    "url": "/kueriot-webapp/static/js/main.13faad7d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/kueriot-webapp/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "150b6ad7438f11cad3b1",
+    "url": "/kueriot-webapp/static/js/runtime~main.150b6ad7.js"
+  },
+  {
+    "revision": "ba58788ad319a60aaf97b3d0f4756367",
+    "url": "/kueriot-webapp/index.html"
+  }
+];
