f214993764f4993ad8ade9bb72a00ec48fc59620
Uraibeef K <uraibeefk@192.168.2.39>
2022-10-10 23:56:51 +0700

    reedit #jumlang to #about

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default class Header extends Component {
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#jumlang">JUMLANG</a></li>
+               <li><a className="smoothscroll" href="#about">JUMLANG</a></li>
              <li><a className="smoothscroll" href="#fento">FENTO</a></li>
                <li><a className="smoothscroll" href="#caesarbot">CAESARBot</a></li>
                <li><a className="smoothscroll" href="#others">Others</a></li>
